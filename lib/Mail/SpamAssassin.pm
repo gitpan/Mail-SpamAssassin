551284c014eb1c2e861c536e5752eef4823d854d
Justin Mason <jmason@cpan.org>
2006-10-10 10:42:03 +0000

    Import of JMASON/Mail-SpamAssassin-3.1.7 from CPAN.
    gitpan-cpan-distribution: Mail-SpamAssassin
    gitpan-cpan-version:      3.1.7
    gitpan-cpan-path:         JMASON/Mail-SpamAssassin-3.1.7.tar.gz
    gitpan-cpan-author:       JMASON
    gitpan-cpan-maturity:     released

--- lib/Mail/SpamAssassin.pm
+++ lib/Mail/SpamAssassin.pm
@@ -95,20 +95,20 @@ use vars qw{
   @site_rules_path
 };
 
-$VERSION = "3.001006";      # update after release (same format as perl $])
-#$IS_DEVEL_BUILD = 1;        # change for release versions
+$VERSION = "3.001007";      # update after release (same format as perl $])
+# $IS_DEVEL_BUILD = 1;        # change for release versions
 
 @ISA = qw();
 
 # SUB_VERSION is now just <yyyy>-<mm>-<dd>
-$SUB_VERSION = (split(/\s+/,'$LastChangedDate: 2006-10-03 21:56:19 -0700 (Tue, 03 Oct 2006) $ updated by SVN'))[1];
+$SUB_VERSION = (split(/\s+/,'$LastChangedDate: 2006-10-05 10:59:08 -0700 (Thu, 05 Oct 2006) $ updated by SVN'))[1];
 
 # If you hacked up your SA, you should add a version_tag to your .cf files.
 # This variable should not be modified directly.
 @EXTRA_VERSION = qw();
 if (defined $IS_DEVEL_BUILD && $IS_DEVEL_BUILD) {
   push(@EXTRA_VERSION,
-       ('r' . qw{$LastChangedRevision: 452727 $ updated by SVN}[1]));
+       ('r' . qw{$LastChangedRevision: 453304 $ updated by SVN}[1]));
 }
 
 sub Version {
